8080ffc167df974f263c516970ea86ef88c529aa
Maat9090 <Maat9090@gmail.com>
2023-01-24 12:23:54 +0100

    Update 7

--- Jenkinsfile
+++ Jenkinsfile
@@ -26,12 +26,7 @@ pipeline{
                 	bat "mvn clean verify"
     } // withMaven will discover the generated Maven artifacts, JUnit Surefire & FailSafe reports and FindBugs reports
 		}}
-	
 
-      
-	    
-	    
-	    
 	    
         stage("Cucumber Report"){
 		steps{
--- src/test/java/tests/TestRunner.java
+++ src/test/java/tests/TestRunner.java
@@ -1,11 +1,5 @@
 package tests;
 
-
-
-
-
-
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
@@ -22,6 +16,8 @@ import io.cucumber.testng.CucumberOptions;
 
 )
 
+
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
+
